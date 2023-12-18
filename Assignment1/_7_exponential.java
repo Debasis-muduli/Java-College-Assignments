67d7367babcaf4dd483c252cb485645bc0a1f2bb
Debasis Muduli <debasismuduli48@gmail.com>
2023-12-18 21:06:11 +0530

    updated code of exponential series

--- Assignment1/_7_exponential.java
+++ Assignment1/_7_exponential.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class _7_exponential {
-    static int fact(int n) {
+    static double fact(int n) {
         int facto=1;
         if (n == 0 || n == 1) {
             return 1;
@@ -14,14 +14,14 @@ public class _7_exponential {
         return facto;
     }
 
-    static int power(int n,int x) {
-        int pow=1;
+    static double power(double x,int n) {
+        double pow=1;
         if (n == 0) {
             pow = 1;
             return pow;
         } else {
             for (int i = 1; i <= n; i++) {
-                pow = pow * i;
+                pow = pow * x;
             }
             return pow;
         }
@@ -29,15 +29,16 @@ public class _7_exponential {
     }
 
     public static void main(String[] args){
-        int n,x,sum=0;
+        int n;
+        double x,sum=0;
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter no. of terms:");
         n=sc.nextInt();
         System.out.println("Enter the value of X:");
-        x=sc.nextInt();
+        x=sc.nextDouble();
         for(int i=0;i<n;i++){
-            sum=sum+(pow(x,i)/fact(i));
+            sum=sum+(power(x,i)/fact(i));
         }
-        System.out.println(sum);
+        System.out.printf("%.2f",sum);
     }
 }
