518ee355dd5d563ef72c8bb2d9f6785043ee0d12
Debasis Muduli <debasismuduli48@gmail.com>
2023-12-21 08:49:57 +0530

    added ques 3 code which shows override method

---
+++ Assignment6/_3_shape.java
@@ -0,0 +1,63 @@
+class Shape {
+    public void draw() {
+        System.out.println("Drawing Shape");
+    }
+
+    public void erase() {
+        System.out.println("Erasing Shape");
+    }
+}
+
+class Circle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Circle");
+    }
+
+    @Override
+    public void erase() {
+        System.out.println("Erasing Circle");
+    }
+}
+
+class Triangle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Triangle");
+    }
+
+    @Override
+    public void erase() {
+        System.out.println("Erasing Triangle");
+    }
+}
+
+class Square extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Square");
+    }
+
+    @Override
+    public void erase() {
+        System.out.println("Erasing Square");
+    }
+}
+
+public class _3_shape {
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        Shape triangle = new Triangle();
+        Shape square = new Square();
+
+        circle.draw();
+        circle.erase();
+
+        triangle.draw();
+        triangle.erase();
+
+        square.draw();
+        square.erase();
+    }
+}
+
