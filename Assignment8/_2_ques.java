215e765915131391b0e7c01043c92d5e8a09778b
Debasis Muduli <debasismuduli48@gmail.com>
2024-01-04 09:11:47 +0530

    Added solution to ques 2 of Assignment 8

---
+++ Assignment8/_2_ques.java
@@ -0,0 +1,57 @@
+class SquareThread extends Thread {
+    int number;
+
+    SquareThread(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        int square = number * number;
+        System.out.println("Thread 1: Square of " + number + " is " + square);
+        //  try {
+        //     Thread.sleep(1000);
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+    }
+}
+
+class DigitSumThread extends Thread {
+    int square;
+
+    DigitSumThread(int square) {
+        this.square = square;
+    }
+
+    @Override
+    public void run() {
+         try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        int digitSum = calculateDigitSum(square);
+        System.out.println("Thread 2: Sum of digits of the square is " + digitSum);
+    }
+
+    int calculateDigitSum(int num) {
+        int sum = 0;
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+}
+
+public class _2_ques {
+    public static void main(String[] args) {
+        int inputNumber = 123;
+
+        new SquareThread(inputNumber).start();
+
+        new DigitSumThread(inputNumber * inputNumber).start();
+
+    }
+}
