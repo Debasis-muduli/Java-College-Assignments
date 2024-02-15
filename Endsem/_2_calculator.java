dbbf72b880ba9ed87af77a9a3eda54a02fec5c62
Debasis Muduli <debasismuduli48@gmail.com>
2024-02-16 00:30:06 +0530

    .

---
+++ Endsem/_2_calculator.java
@@ -0,0 +1,40 @@
+interface Calculator{
+    double add(double a,double b);
+    double sub(double a,double b);
+    double mul(double a,double b);
+    double div(double a,double b);
+}
+class DemoCalculator implements Calculator {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double sub(double a, double b) {
+        return a - b;
+    }
+
+    public double mul(double a, double b) {
+        return a * b;
+    }
+
+    public double div(double a, double b) {
+        return a / b;
+    }
+}
+
+public class _2_calculator {
+    public static void main(String[] args) {
+        DemoCalculator calculator = new DemoCalculator();
+        System.out.println();
+        double sum = calculator.add(5, 3);
+        double difference = calculator.sub(5, 3);
+        double product = calculator.mul(5, 3);
+        double quotient = calculator.div(5, 3);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+
+    }
+}
